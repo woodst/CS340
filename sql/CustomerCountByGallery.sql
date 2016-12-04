d7df243edad4cf7a9e01f9f4b627bd33a3022439
Thomas M Woods <woodsth@regonstate.edu>
2016-12-04 11:45:40 -0800

    new file:   CustomerCountByGallery.sql
    Added customer count by gallery with order by gallery name

---
+++ sql/CustomerCountByGallery.sql
@@ -0,0 +1,24 @@
+-- ----------------------------------------------------------
+-- CS340 Intrdoduction to Datbases
+-- Fall 2016
+-- Class Project
+-- Joe Valencia
+-- Tom Woods
+-- ----------------------------------------------------------
+-- CustomerCount
+-- ----------------------------------------------------------
+
+-- Reference for the Dev Database on AWS.  Comment out for deployment elswhere
+use CS340;
+
+-- Copy line 16 through 18 to php
+set @statement = '
+SELECT g.galleryName, count(c.customerID) from gallery g 
+JOIN section s ON s.galleryID = g.galleryID JOIN artwork a ON s.sectionID = a.artworkSectionID JOIN sales sa ON a.artworkID = sa.artworkID JOIN customer c ON sa.customerID = c.customerID
+GROUP BY g.galleryName ORDER BY galleryName
+';
+
+-- test
+prepare stmt from @statement;
+
+execute stmt;
