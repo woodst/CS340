a459a100c8b45328c23621830e48cf903556653a
J-VAL <valencjo@oregonstate.edu>
2016-12-05 01:54:51 -0800

    added changes to purchaseArt.php.  Added final project word doc

--- php/purchaseArt.php
+++ php/purchaseArt.php
@@ -1,32 +1,35 @@
 <?php
+$worked = false;
 ini_set('display_errors', 'On');
 $mysqli = new mysqli("oniddb.cws.oregonstate.edu","woodsth-db","jDJ3RibroohRXZkT","woodsth-db");
 if($mysqli->connect_errno){
 	echo "Connection Error no. " . $mysqli->connect_errno . ", " . $mysqli->connect_error;
 	}
 	
-if(!($stmt = $mysqli->prepare("INSERT INTO sales (saleDescription, artworkID, customerID VALUES (?,?,?)  "))){
+if(!($stmt = $mysqli->prepare("INSERT INTO sales (saleDescription, artworkID, customerID ) 
+VALUES (?,?,(select customerID from visitorLog where customerID = ? and sectionID = (select artworkSectionID from artwork where artworkID = ? limit 1 )))"))){
 	echo "Prepare failed. Error no. "  . $stmt->errno . ", " . $stmt->error;
 }
-if(!($stmt->bind_param("sii",$_POST['desc'],$_POST['Customers'],$_POST['Artwork']))){
+if(!($stmt->bind_param("siii",$_POST['desc'],$_POST['Artwork'],$_POST['Customers'],$_POST['Artwork']))){
 	echo "Bind failed. Error no. "  . $stmt->errno . ", " . $stmt->error;
 }
+
 if(!$stmt->execute()){
 	echo "Execute failed. Error no. "  . $stmt->errno . " " . $stmt->error;
 } else {
-	echo "Added " . $stmt->affected_rows . " rows to sales";
+	echo "Added " . $stmt->affected_rows . " rows to sales\n";
+	$worked = true;
 }
 $stmt->close();
-?>
 
-<?php
-ini_set('display_errors', 'On');
-$mysqli = new mysqli("oniddb.cws.oregonstate.edu","woodsth-db","jDJ3RibroohRXZkT","woodsth-db");
+
+
+if($worked){
 if($mysqli->connect_errno){
 	echo "Connection Error no. " . $mysqli->connect_errno . ", " . $mysqli->connect_error;
 	}
 	
-if(!($stmt = $mysqli->prepare(UPDATE artwork SET isSold=1 WHERE artworkID=?))){
+if(!($stmt = $mysqli->prepare("UPDATE artwork SET isSold=1 WHERE artworkID=?"))){
 	echo "Prepare failed. Error no. "  . $stmt->errno . ", " . $stmt->error;
 }
 if(!($stmt->bind_param("i",$_POST['Artwork']))){
@@ -38,4 +41,6 @@ if(!$stmt->execute()){
 	echo "Updated " . $stmt->affected_rows . " rows in artwork";
 }
 $stmt->close();
+}
+
 ?>
